6906f9d4f28cac5f800a7689e04d2a2edf52981e
Bharat Monavarthi <Bharat Monavarthi>
2017-05-18 16:33:19 +0530

    Added WeatherMessage component that shows the weather (static placeholder for now)

---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class WeatherMessage extends React.Component {
+  render() {
+    return(
+      <p>It is 41 C in Hyderabad</p>
+    );
+  }
+}
+
+module.exports = WeatherMessage;
