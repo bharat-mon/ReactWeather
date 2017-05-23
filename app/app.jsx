819ac9ad7c9ca8d68d348fbae606d9e7462a17e0
Bharat Monavarthi <bharat.monavarthi@hotmail.com>
2017-05-23 13:48:12 +0530

    Added foundation and jquery dependencies

--- app/app.jsx
+++ app/app.jsx
@@ -7,6 +7,9 @@ import Weather from 'Weather';
 import About from 'About';
 import Examples from 'Examples';
 
+require('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router>
     <div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    './app/app.jsx',
+    'script-loader!jquery/dist/jquery.min.js',
+    'script-loader!foundation-sites/dist/js/foundation.min.js'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
