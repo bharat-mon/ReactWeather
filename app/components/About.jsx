9b0fd355f3f94baba7af083cad74e6ad17fa7d15
Bharat Monavarthi <bharat.monavarthi@hotmail.com>
2017-05-24 17:33:21 +0530

    Updated the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,9 +3,12 @@ import React, {Component} from 'react';
 const About = (props) => {
   return (
     <div className="row">
-      <div className="medium-6 large-4 small-centered columns">
-        <h3>About</h3>
-        <p>Welcome to the About page!</p>
+      <div className="medium-6 large-4 small-centered columns text-center">
+        <h1>About</h1>
+        <p>A Weather app made using React</p>
+        <p>Tools used in this application:</p>
+        <a href="https://facebook.github.io/react/" target="_blank">React - A JavaScript library for building user interfaces</a><br /><br />
+        <a href="https://openweathermap.org" target="_blank">OpenWeatherMap - Сurrent weather and forecast</a>
       </div>
     </div>
   );
