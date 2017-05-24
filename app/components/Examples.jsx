bd3790be952aef7fd2c4c3b0d3cdceee387f8501
Bharat Monavarthi <bharat.monavarthi@hotmail.com>
2017-05-24 17:00:14 +0530

    Added dummy example links

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 const Examples = (props) => {
   return (
-    <div>
-      <h3>Examples</h3>
-      <p>Welcome to the examples page!</p>
+    <div className="row">
+      <div className="medium-6 large-4 small-centered columns">
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out:</p>
+        <ol>
+          <li><Link to="/?location=Hyderabad">Hyderabad, IN</Link></li>
+          <li><Link to="/?location=Dubai">Dubai, UAE</Link></li>
+        </ol>
+      </div>
     </div>
   );
 };
